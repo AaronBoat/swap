88be3965f3b8ed97db08ddc4fcc5f8b9cb916a26
AaronBoat <aboateng7@gmail.com>
2014-11-16 13:56:31 +0000

    Start swap app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swap::Application.config.secret_key_base = '25377f12633ac4ef8b92ecd2b334cea35d45da37fc2c2830f94c8000b801bffab559ae543380c3181a4e9e86bd0017449d791be71d68126feddb33337c2228b9'
